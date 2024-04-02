78284d0c00100bf6fabebfde2618bf523453a232
Pulkit Jain <142376515+jainpulkit6358@users.noreply.github.com>
2024-04-02 15:37:14 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,102 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+//                  factorial
+// int main()
+// {
+//     int n,i,fact=1;
+//     printf("Enter a Number");
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++){
+//         fact*=i;
+//     }
+//     printf("%d! =%d",n,fact);
+// }
+
+
+
+
+//                  Armstrong
+
+
+
+// int power(int a,int b){
+//     int power = 1 , i ;  
+//     for ( i = 1 ; i <= b ; + + i ) {  
+//         power = power * a ;  
+//     }  
+//     return power ; 
+// }
+// int main(){
+    
+// int num,i, originalNum,result=0, remainder,m=0;
+
+//     printf("Enter a  integer: ");
+//     scanf("%d", &num);
+//     originalNum = num;
+//     while (originalNum != 0) {
+       
+//       originalNum /= 10;
+//         m++;
+//     }
+// originalNum = num;
+//     while (originalNum != 0) {
+       
+//         remainder = originalNum % 10;
+        
+//          result += power(remainder, m);
+      
+        
+//       originalNum /= 10;
+//     }
+
+//     if (result == num)
+//         printf("%d is an Armstrong number.", num);
+//     else
+//         printf("%d is not an Armstrong number.", num);
+// return 0;
+// }
+
+
+
+//                      Perfect 
+
+int main() {
+    // Write C code here
+   int n,i,s=0;
+   printf("Enter a Number");
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++){
+        if(n%i==0){
+            s+=i;
+        }
+    }
+    if(s==n)
+    printf("%d is a perfect number",n);
+    else
+    printf("%d not perfect Number",n);
+    return 0;
+}
+
+
+//                  Reverse
+int main() {
+    int n,i,sum=0,rem;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(n!=0){
+        rem=n%10;
+        sum=sum*10+rem;
+        n=n/10;
+    }
+    printf("%d is reverse",sum);
+    
+}
+
