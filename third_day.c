5754a5602e41d19c4a44430ad6393a24c8e6e7e0
Pulkit Jain <142376515+jainpulkit6358@users.noreply.github.com>
2024-04-04 10:52:03 +0530

    Create third_day.c

---
+++ third_day.c
@@ -0,0 +1,82 @@
+// #include <stdio.h>
+
+// int main() {
+//     int a[3]={2,3,4},b[3]={2,3,4};
+//     if(a==b){
+    
+//         printf("equal");
+//     }
+//     else{
+//         printf("not Equal");
+//     }
+
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// int main() {
+//     const int a=10;
+//     printf("%d",++a);
+
+//     return 0;
+// }
+
+
+
+
+// Online C compiler to run C program online
+// #include <stdio.h>
+
+// int factorial(int n) {
+//    int f;
+   
+//    for(f = 1; n > 1; n--)
+//       f *= n;
+      
+//    return f;
+// }
+
+// int ncr(int n,int r) {
+    
+//    return factorial(n) / ( factorial(n-r) * factorial(r) );
+// }
+
+// int main() {
+//    int a,n,r, i, j;
+
+//    scanf("%d",&a);
+
+//    for(i = 0; i < a; i++) {
+//       for(j = 0; j < a-i; j++)
+//          printf("  ");
+         
+//       for(j = 0; j <= i; j++)
+//          printf(" %3d", ncr(i, j));
+
+//       printf("\n");
+//    }
+   
+//    printf("row: and Column");
+//    scanf("%d%d",&n,&r);
+//      for(i = 0; i <= a; i++) {
+         
+//       for(j = 0; j <= i; j++){
+//           if(n==i&&r==j){
+              
+//               printf("the element is %d", ncr(i-1, j-1));
+//               break;
+//           }
+//       }
+//      }  
+
+     
+   
+//    return 0;
+
+// }
+
+
+
